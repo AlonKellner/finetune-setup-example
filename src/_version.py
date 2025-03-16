b9495cf01019a30497342a10d98f38fb4408bb2a
Alon Kellner <me@alonkellner.com>
2025-03-16 09:52:43 +0000

    fix: valid pyproject.toml

--- src/_version.py
+++ src/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev26+gb4f3b4e.d20250309'
-__version_tuple__ = version_tuple = (0, 1, 'dev26', 'gb4f3b4e.d20250309')
+__version__ = version = '0.1.dev34+g12d294f.d20250316'
+__version_tuple__ = version_tuple = (0, 1, 'dev34', 'g12d294f.d20250316')
