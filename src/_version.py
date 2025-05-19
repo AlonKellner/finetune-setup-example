77af230b17fa03f8595de020e9d05f42bf1087d0
Alon Kellner <me@alonkellner.com>
2025-05-19 16:45:00 +0000

    feat: uv self update works

--- src/_version.py
+++ src/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev68+ge70342a.d20250518'
-__version_tuple__ = version_tuple = (0, 1, 'dev68', 'ge70342a.d20250518')
+__version__ = version = '0.1.dev69+g8a4325f.d20250519'
+__version_tuple__ = version_tuple = (0, 1, 'dev69', 'g8a4325f.d20250519')
