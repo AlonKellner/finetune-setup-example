cf5ab4bddf31818df732c7ab21b6ec8e76856318
Alon Kellner <me@alonkellner.com>
2025-05-29 16:11:14 +0000

    fix: better print

--- src/mms_blog_post.hp_main.py
+++ src/mms_blog_post.hp_main.py
@@ -1,5 +1,7 @@
 """A real example of multi-HP jobs."""
 
+import json
+
 from finetune_setup_example.hp_set_handling import get_defaults
 from finetune_setup_example.mains.mms_common_voice_adaptune import main
 from finetune_setup_example.remote_job_utils import start_hp_jobs
@@ -14,7 +16,8 @@ def hp_main() -> None:
         dict(num_training_steps=num_training_steps, num_train_epochs=None)
         for num_training_steps in [100, 200, 300]
     ]
-    print(f"Created {len(hp_sets)} hyper-parameter sets:\n{hp_sets}")
+    full_json = json.dumps(dict(enumerate(hp_sets)), indent=2)
+    print(f"Created {len(hp_sets)} hyper-parameter sets:\n{full_json}")
     job_names = start_hp_jobs(
         f"{name}.job.yaml", default_hp_set, hp_sets, f"{name}.env"
     )
