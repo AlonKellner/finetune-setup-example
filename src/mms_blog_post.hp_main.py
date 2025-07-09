9020c1db66884addf6ca750903ac0e24f0e6b3f8
AlonKellner-Jounce <alon@jounce.io>
2025-07-09 18:05:08 +0000

    auto: .

--- src/mms_blog_post.hp_main.py
+++ src/mms_blog_post.hp_main.py
@@ -32,7 +32,7 @@ def hp_main() -> None:
             per_device_eval_batch_total_seconds=batch_total_seconds,
         )
         for batch_total_seconds in [2100.0]
-        for job_type in ["mi300x"]
+        for job_type in ["a100"]
     ]
     full_json = json.dumps(dict(enumerate(hp_sets)), indent=2)
     print(f"Created {len(hp_sets)} hyper-parameter sets:\n{full_json}")
